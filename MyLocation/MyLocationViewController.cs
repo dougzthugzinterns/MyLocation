ac72b2d961625b06c6c8830d6ff7d62a99ab434e
dougzthugzinterns <freerjm@egb-10-33-10-193.muo.muohio.edu>
2013-06-25 15:38:27 -0400

    Maps with Pins
    I added in a pin dropper method.

--- MyLocation/MyLocationViewController.cs
+++ MyLocation/MyLocationViewController.cs
@@ -2,13 +2,16 @@ using System;
 using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using MonoTouch.CoreLocation;
 using Google.Maps;
+using System.Threading;
 
 namespace MyLocation
 {
 	public partial class MyLocationViewController : UIViewController
 	{
 		MapView mapView;
+
 
 		public MyLocationViewController () : base ("MyLocationViewController", null)
 		{
@@ -19,14 +22,23 @@ namespace MyLocation
 		{
 			base.LoadView ();
 
-			//var camera = CameraPosition.FromCamera (latitude: 37.797865, 
-			                                        //longitude: -122.402526, 
-			                                        //zoom: 6);
-			//mapView = MapView.FromCamera (RectangleF.Empty, camera);
-			mapView.MyLocationEnabled = true;
-			var camera = CameraPosition.FromCamera (mapView.MyLocation.Coordinate.Latitude, mapView.MyLocation.Coordinate.Longitude, 6);
+			var camera = CameraPosition.FromCamera (latitude: 37.797865, 
+			                                        longitude: -122.402526, 
+			                                        zoom: 2);
 			mapView = MapView.FromCamera (RectangleF.Empty, camera);
+			
+			mapView.MyLocationEnabled = true;
+			//double latitude = mapView.MyLocation.Coordinate.Latitude;
+			//double longitude = mapView.MyLocation.Coordinate.Longitude;
+
 			View = mapView;
+
+			for(int i = 0; i <10; i++){
+
+			addPinToCurrentLocation();
+
+			View = mapView;
+			}
 		}
 
 		public override void ViewWillAppear (bool animated)
@@ -40,6 +52,28 @@ namespace MyLocation
 			mapView.StopRendering ();
 			base.ViewWillDisappear (animated);
 		}
+		public void addPinToCurrentLocation ()
+		{
+			CLLocation myPosition;;
+			myPosition = mapView.MyLocation;
+			if (myPosition == null) {
+				Console.WriteLine ("Cant resolve location.");
+			}else{
+				Console.WriteLine ("I got you bitch");
+			}
+			double mylat = myPosition.Coordinate.Latitude;
+			double mylong = myPosition.Coordinate.Longitude;
+			CLLocationCoordinate2D myPos = new CLLocationCoordinate2D (mylat, mylong);
+
+			var myPostionMarker = new Marker () {
+				Title = "My location",
+				Snippet = "",
+				Position = myPos,
+				Map = mapView
+			};
+
+			mapView.SelectedMarker = myPostionMarker;
+			Thread.Sleep (10000);
 	}
 }
-
+}
